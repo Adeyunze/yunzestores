f3e52997a588a48cf443b2895415b898aded82c5
Adeyunze <adeyunze05@gmail.com>
2022-04-24 20:43:11 +0100

    Fixed Max width

--- src/components/pages/Men.jsx
+++ src/components/pages/Men.jsx
@@ -3,7 +3,7 @@ import {MenProduct} from '../WomenProduct'
 
 const Men = () => {
     return (
-        <div className=''>
+        <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
             <div className=''></div>
             <MenProduct/>
         </div>
--- src/components/pages/Women.jsx
+++ src/components/pages/Women.jsx
@@ -3,7 +3,7 @@ import {WomenProduct} from '../WomenProduct'
 
 const Women = () => {
     return (
-        <div className=''>
+        <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
             <div className=''></div>
             <WomenProduct/>
         </div>
